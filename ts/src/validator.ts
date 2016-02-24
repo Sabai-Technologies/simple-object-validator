adb553ff86985473351d65b495c8aef9b49161ab
camory <christophe@amory.fr>
2016-02-24 18:47:57 +0100

    added doc for validator methods

--- ts/src/validator.ts
+++ ts/src/validator.ts
@@ -7,11 +7,20 @@ import R = require('ramda');
 import KeyValuePair = R.KeyValuePair;
 import CurriedFunction2 = R.CurriedFunction2;
 
-export var validate:CurriedFunction2<Schema, any, Validation> = R.curry(
-    (schema:Schema, obj:any):Validation => {
+
+/**
+ * Validate the given object according the given Schema.
+ * If all the indexed properties, defined in the Schema, are successfully validated, then returns a Success
+ * with the given object as context otherwise returns a Failure containing an object which value of its indexed properties
+ * is an array of error message.
+ *
+ * validate :: Schema -> {[index:string]} -> Validation
+ */
+export var validate = R.curry(
+    (schema:Schema, obj:{[index:string]:any}):Validation => {
         if (R.is(Object, obj)) {
             let validations:Validation[] = R.compose(R.map(convert), R.toPairs, validateObj(schema))(obj);
-            let validation = reduce(R.prepend(Validation.of(R.curryN(validations.length, R.identity)), validations));
+            let validation = reduce(R.prepend(Validation.of(identityN(validations.length)), validations));
 
             return validation.bimap(R.compose(R.of, R.mergeAll), () => obj);
         }
@@ -20,28 +29,64 @@ export var validate:CurriedFunction2<Schema, any, Validation> = R.curry(
     }
 );
 
-const validateObj:CurriedFunction2<Schema,{[index:string]:any}, {[index:string]:Validation}> = R.curry(
-    (schema:Schema, object:{[index:string]:any}):{[index:string]:Validation} => {
-        return R.mapObjIndexed(validateProp(schema), R.pick(R.keys(schema), object))
-    }
+/**
+ * Return a curried identity function with the given arity
+ *
+ * identityN :: n
+ */
+const identityN = (n:number) => R.curryN(n, R.identity);
+
+/**
+ * Apply validation of the indexed properties defined in the given Scheam to the value of the same indexed properties
+ * defined in the given
+ *
+ * validateObj :: Schema -> {[index:string]:any} -> {[index:string]:Validation}
+ */
+const validateObj = R.curry(
+    (schema:Schema, obj:{[index:string]:any}):{[index:string]:Validation} => R.mapObjIndexed(validateProp(schema), pick(schema, obj))
 );
 
-const validateProp = (schema:Schema):(value:any, key:string) => Validation => {
-    return (value:any, key:string):Validation => {
-        let validations:Validation[] = R.ap(schema[key].validations, R.of(value));
 
-        return reduce(R.prepend(Validation.of(R.curryN(validations.length, ()=>value)), validations));
-    }
-};
+/**
+ * Remove all the attributes of the given literal object which are not defined in the given Schema
+ *
+ * pick :: Schema -> {[index:string]:any} -> {[index:string]:any}
+ */
+const pick = R.curry((schema:Schema, obj:{[index:string]:any}):{[index:string]:any} => R.pick(R.keys(schema), obj));
 
-const convert = (pair:KeyValuePair<string, Validation>):Validation => {
-    return pair[1].bimap(R.compose(R.of, objFrom(pair[0])), () => pair[0]);
-};
+/**
+ * Validate the value of a key according the validations defined in the Schema
+ *
+ * validateProp :: Schema -> any -> string -> Validation
+ */
+const validateProp = R.curry((schema:Schema, value:any, key:string):Validation => {
+    let validations:Validation[] = R.ap(schema[key].validations, R.of(value));
 
-const objFrom:CurriedFunction2<string, any, {[index: string]: any}> = R.curry(
-    (key:string, value:any):{[index: string]: any} => R.fromPairs([[key, value]])
-);
+    return reduce(R.prepend(Validation.of(R.curryN(validations.length, ()=>value)), validations));
+});
 
+/**
+ * Bimap the Validation of the given KeyValuePair.
+ * If the validation is a Failure then map it with a function creating a literal object with only one indexed property
+ * and which name is the given string.
+ * If the validation is a Success, then map it with the identity function
+ *
+ *  convert :: [string, Validation] -> Validation
+ */
+const convert = (pair:KeyValuePair<string, Validation>):Validation => pair[1].bimap(R.compose(R.of, toObj(pair[0])), R.identity);
+
+/**
+ * Create literal object with only one indexed property which name is the given key and the value the given value.
+ *
+ * toObj :: string -> any -> {string:any}
+ */
+const toObj = R.curry((key:string, value:any):{[index: string]: any} => R.fromPairs([[key, value]]));
+
+/**
+ * Reduce a collection of Validation par applying the 'ap' method
+ *
+ * reduce :: [Validation] -> Validation
+ */
 const reduce = (validations:Validation[]):Validation =>
     R.reduce(
         (val1:Validation, val2:Validation):Validation => val1.ap(val2),
